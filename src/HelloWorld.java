f6615fe71970cf116ed823c8cde1bfa22857f63b
antonydrift <antonydrift96@gmail.com>
2018-12-11 21:04:39 +0300

    Merge branch 'master' of /home/antonydrift/IdeaProjects/dsss/TestGit with conflicts.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World!!!!!");
-
-
-=======
         System.out.println("Hello World!");
 
         //some text
->>>>>>> commit for branch 11111
     }
 }
